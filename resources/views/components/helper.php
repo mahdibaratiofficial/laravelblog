916f135cb564fe89776fb0eb3f30544a50c019e7
Mahdi <mahdibarati696@gmail.com>
2022-05-31 09:40:58 +0430

    custome Helper Added

--- resources/views/components/helper.php
+++ resources/views/components/helper.php
@@ -1,14 +1,15 @@
 <?php
 
-if(!function_exists('isset_product')){
-    function isset_product($argu){
-        if(isset($argu) && !empty($argu)){
-            return $argu;
-        }else{
-            return null;
+if (!function_exists('isset_product')) {
+    function isset_product($argu)
+    {
+        if ($argu) {
+            if (isset($argu) && !empty($argu)) {
+                return $argu;
+            } else {
+                return "";
+            }
         }
     }
 }
-
-
 ?>
