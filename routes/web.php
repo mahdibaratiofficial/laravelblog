9c1f70310f8bdf4fec4c21514047dfaccf2f798c
Mahdi <mahdibarati696@gmail.com>
2022-05-31 09:40:32 +0430

    Change Routes and Connect to Controller

--- routes/web.php
+++ routes/web.php
@@ -12,124 +12,24 @@
 */
 
 // use Illuminate\Routing\Route;
-
-use App\Products;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Validator;
-use Symfony\Component\Process\Process;
-
-Route::get('/', function () {
-    return view('index');
-});
-
+/*
+PagesController : this Controller used For Open Pages
+*/
+Route::get('/', "PagesController@index");
+Route::get('content',"PagesController@Content");
+Route::get('cart', "PagesController@Cart");
+Route::get('checkout', "PagesController@Checkout");
+Route::get('test', "PagesController@Test");
 Route::prefix('shop')->group(function () {
-    Route::get('/', function () {
-        return view('shop');
-    });
-    Route::get('show/{slug}', function ($slug) {
-        return view('showPost', ['slug' => $slug]);
-    });
+    Route::get('/', "PagesController@shop");
+    Route::get('show/{ProductSlug}', "PagesController@singleProduct");
 });
-
-Route::get('content', function () {
-    return view('content');
-});
-Route::get('cart', function () {
-    return view('cart');
-});
-Route::get('checkout', function () {
-    return view('checkout');
-});
-Route::get('test', function () {
-    /* $name=DB::table('products')->select('product_name','product_description')->groupBy('product_name','product_description')->get();
-    dd($name);*/
-
-    /* $name=DB::table('users')->insertOrIgnore(
-        [
-            'name'=>"mahdi",
-            'email'=>"mahdibarati".rand(rand(1,4),rand(4,9))."@gmail.com",
-            'password'=>'123456'
-        ]
-    );*/
-    // $name=DB::table('users')->where('email','mahdibarati696@gmail.com')->update(['name'=>'Mahdi Barati']);
-    $name = DB::table('users')->select('name')->where('id', 2)->dd();
-    return view('test', ['name' => $name]);
-});
-
-
-Route::prefix('admin')->group(function () {
-    Route::get('/', function () {
-        return view('admin.index');
-    });
-
-    Route::get('/addproduct', function () {
-        return view('admin.pages.addproduct', ['insert' => true]);
-    });
-
-    Route::post('/product/insert', function () {
-
-        $validator = Validator::make(request()->all(), [
-            'product' => 'required|min:5|max:50',
-            'types' => 'required|min:5|max:20',
-            "product_description" => "required",
-            "price" => 'between:3,7',
-            "gender" => "required",
-            "photos" => "required",
-            "size" => "required|min:1|max:5",
-            "colors" => "required|min:2|max:10",
-            "count" => 'required|numeric',
-        ])->validated();
-
-        Products::create([
-            'product' => $validator['product'],
-            'slug' => $validator['product'],
-            'type' => $validator['types'],
-            'product_description' => $validator['product_description'],
-            'price' => $validator['price'],
-            'photos' => $validator['photos'],
-            'gender' => $validator['gender'],
-            'size' => $validator['size'],
-            "color" => $validator['colors'],
-            'count' => $validator['count'],
-        ]);
-    });
-
-    Route::get('edit/{product}', function ($product) {
-        $pro = Products::findOrFail($product)->first();
-
-        dd($pro);
-        return view('admin.pages.addproduct', [
-            'product' => $pro,
-            'edit' => true
-        ]);
-    });
-
-    Route::post('/update/{product}', function ($product) {
-        $pro = Products::findOrFail($product);
-        $validator = Validator::make(request()->all(), [
-            'product' => 'required|min:5|max:50',
-            'types' => 'required|min:5|max:20',
-            "product_description" => "required",
-            "price" => 'between:3,7',
-            "gender" => "required",
-            "photos" => "required",
-            "size" => "required|min:1|max:5",
-            "colors" => "required|min:2|max:10",
-            "count" => 'required|numeric',
-        ]);
-
-        $pro->update([
-            'product' => $validator['product'],
-            'slug' => $validator['product'],
-            'type' => $validator['types'],
-            'product_description' => $validator['product_description'],
-            'price' => $validator['price'],
-            'photos' => $validator['photos'],
-            'gender' => $validator['gender'],
-            'size' => $validator['size'],
-            "color" => $validator['colors'],
-            'count' => $validator['count'],
-        ]);
-        return redirect()->back();
-    });
+Route::prefix('admin')->namespace('Admin')->group(function () {
+    Route::get('/', "PagesController@index");
+    Route::get('/addproduct', "PagesController@addProductPage");
+    Route::post('/product/insert', "ProductController@Insert");
+    Route::get('edit/{ProductSlug}', "PagesController@Edit");
+    Route::post('/update/{product}', "ProductController@Update");
+    Route::get('products',"PagesController@Products");
+    Route::delete('delete/{id}',"ProductController@Delete");
 });
